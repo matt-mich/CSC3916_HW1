7fdd98215bc94d3d95e12d6930ce0b3f232b8214
Matthew Michaelis <matthew.michaelis@ucdenver.edu>
2019-02-17 23:25:20 -0700

    Added server files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CSC3916_HW1
+
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/11de6296ce9b48f8a82d)
--- server.js
+++ server.js
@@ -16,10 +16,6 @@ router.route('/')
     .post(function (req, res) {
             console.log(req.body);
             res = res.status(200);
-            if (req.get('Content-Type')) {
-                console.log("Content-Type: " + req.get('Content-Type'));
-                res = res.type(req.get('Content-Type'));
-            }
             res.send(req.body);
         }
     );
